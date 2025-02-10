7e92a2d785f1ca7f241df07605e19ff4648b491c
Aarushi Gupta <80885332+aarushiig@users.noreply.github.com>
2025-02-10 16:28:38 +0530

    [LC-206] Reverse Linked List (Iteration & Recursion) (#1)

--- src/problems/ReverseLinkedList206.java
+++ src/problems/ReverseLinkedList206.java
@@ -8,9 +8,16 @@ import utility.LinkedListUtilities;
 public class ReverseLinkedList206 {
     public static void main(String[] args) {
         System.out.printf("REVERED LL: %s%n", LinkedListUtilities.getLinkedListAsString(reverseListInPlace(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1, 2, 3, 4, 5}))));
+        System.out.printf("REVERED LL: %s%n%n", LinkedListUtilities.getLinkedListAsString(reverseListUsingRecursion(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1, 2, 3, 4, 5}))));
+
         System.out.printf("REVERED LL: %s%n", LinkedListUtilities.getLinkedListAsString(reverseListInPlace(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1, 2}))));
+        System.out.printf("REVERED LL: %s%n%n", LinkedListUtilities.getLinkedListAsString(reverseListUsingRecursion(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1, 2}))));
+
         System.out.printf("REVERED LL: %s%n", LinkedListUtilities.getLinkedListAsString(reverseListInPlace(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1}))));
+        System.out.printf("REVERED LL: %s%n%n", LinkedListUtilities.getLinkedListAsString(reverseListUsingRecursion(LinkedListUtilities.generateLinkedListFromArray(new Integer[]{1}))));
+
         System.out.printf("REVERED LL: %s%n", LinkedListUtilities.getLinkedListAsString(reverseListInPlace(LinkedListUtilities.generateLinkedListFromArray(new Integer[0]))));
+        System.out.printf("REVERED LL: %s%n%n", LinkedListUtilities.getLinkedListAsString(reverseListInPlace(LinkedListUtilities.generateLinkedListFromArray(new Integer[0]))));
     }
 
     /* CONSTRAINTS: The number of nodes in the list is the range [0, 5000].
@@ -19,18 +26,29 @@ public class ReverseLinkedList206 {
     // TIME COMPLEXITY  : O(n)
     // SPACE COMPLEXITY : O(1)
     private static ListNode reverseListInPlace(ListNode head) {
-        if (head == null || head.next == null) return head;
+        ListNode previous = null, current = head;
 
-        ListNode L = head, M = L.next, R = M.next;
-        while (M != null) {
-            M.next = L;
+        while (current != null) {
+            ListNode next = current.next;
 
-            L = M;
-            M = R;
-            if (R != null) R = R.next;
+            current.next = previous;
+
+            previous = current;
+            current = next;
         }
 
+        return previous;
+    }
+
+    // TIME COMPLEXITY  : O(n)
+    // SPACE COMPLEXITY : O(1)
+    private static ListNode reverseListUsingRecursion(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode newHead = reverseListUsingRecursion(head.next);
+        head.next.next = head;
         head.next = null;
-        return L;
+
+        return newHead;
     }
 }
